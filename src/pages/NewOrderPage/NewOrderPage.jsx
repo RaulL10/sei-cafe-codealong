5d6be419fccbe2d635403fe790060b67114fbb72
RaulL10 <lopezraul1410@gmail.com>
2022-05-05 13:49:22 -0700

    checkout functionality done

--- src/pages/NewOrderPage/NewOrderPage.jsx
+++ src/pages/NewOrderPage/NewOrderPage.jsx
@@ -2,24 +2,24 @@ import { useState, useEffect, useRef } from 'react';
 import * as itemsAPI from '../../utilities/items-api';
 import * as ordersAPI from '../../utilities/orders-api'
 import './NewOrderPage.css';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import Logo from '../../components/Logo/Logo';
 import MenuList from '../../components/MenuList/MenuList';
 import CategoryList from '../../components/CategoryList/CategoryList';
 import OrderDetail from '../../components/OrderDetail/OrderDetail';
 import UserLogOut from '../../components/UserLogOut/UserLogOut';
 
+
+
 export default function NewOrderPage({ user, setUser }) {
   const [menuItems, setMenuItems] = useState([]);
   const [activeCat, setActiveCat] = useState('');
   const [cart, setCart] = useState(null)
-  // Obtain a ref object
   const categoriesRef = useRef([]);
+  const navigate = useNavigate()
 
-  // useEffect(function() {
-  //   console.log('NewOrderPage rendered');
-  // });
   
+
   useEffect(function() {
     async function getItems() {
       const items = await itemsAPI.getAll();
@@ -31,16 +31,36 @@ export default function NewOrderPage({ user, setUser }) {
       setMenuItems(items);
     }
     getItems();
-    async function getCart() {
-      const cart = await ordersAPI.getCart()
-      setCart(cart)
-    }
-    getCart()
 
+    // Load the user's cart (the unpaid order for that user)
+    async function getCart() {
+      const cart = await ordersAPI.getCart();
+      setCart(cart);
+    }
+    getCart();
   }, []);
+
+
   // the empty dependency array above will result in 
   // the function running after the FIRST render
   // only
+/*--- Event Handlers --- */
+async function handleAddToOrder(itemId) {
+  const newCart = await ordersAPI.addItemToCart(itemId);
+  setCart(newCart);
+}
+
+// Add this function
+async function handleChangeQty(itemId, newQty) {
+  const updatedCart = await ordersAPI.setItemQtyInCart(itemId, newQty);
+  setCart(updatedCart);
+}
+
+async function handleCheckout() {
+  await ordersAPI.checkout()
+  navigate('/orders')
+}
+
 
   return (
     <main className="NewOrderPage">
@@ -55,9 +75,10 @@ export default function NewOrderPage({ user, setUser }) {
         <UserLogOut user={user} setUser={setUser} />
       </aside>
       <MenuList
-        menuItems={menuItems.filter(item => item.category.name === activeCat)}
+        menuItems={menuItems.filter(item => item.category.name === activeCat)} 
+        handleAddToOrder={handleAddToOrder}
       />
-      <OrderDetail order={cart} />
+      <OrderDetail order={cart} handleChangeQty={handleChangeQty} handleCheckout={handleCheckout} />
     </main>
   );
 }
